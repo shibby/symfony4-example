06678c476c08a94f14f63b36150eb03d1ae5bc43
shibby <guvenatbakan@gmail.com>
2017-12-07 14:36:46 +0300

    add forgotten unique constraint

--- src/Entity/Stock.php
+++ src/Entity/Stock.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\StockRepository")
+ * @ORM\Table(uniqueConstraints={@ORM\UniqueConstraint(name="stock_unique",columns={"company_id", "market_id", "type"})})
  */
 class Stock
 {
